4bdddffd43ea5e5d62b8e222fe3b541fa1243206
Chand <cdeshwal@gmail.com>
2026-08-22 18:20:13 +0800

    release: pin collector installers to v0.2.2 digest

--- install-openshift.sh
+++ install-openshift.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 CONTROL_PLANE_URL=""; ORG_ID=""; INTEGRATION_ID=""; ENROLLMENT_TOKEN=""
-IMAGE="${COLLECTOR_IMAGE:-ghcr.io/cloudverse-pte-ltd/cloudverse-data-center-collector@sha256:b3ce2908efb464a53fa62bd81bb85b2669a5309fb82f5daf8434381a7a86db09}"
+IMAGE="${COLLECTOR_IMAGE:-ghcr.io/cloudverse-pte-ltd/cloudverse-data-center-collector@sha256:2fb62ab78725a5ee98ceac2382cf3c9098113d479bee52d75ada69f77bb52a33}"
 while [ "$#" -gt 0 ]; do
   case "$1" in
     --control-plane-url) CONTROL_PLANE_URL="$2"; shift 2 ;;
